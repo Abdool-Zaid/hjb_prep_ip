b222e018f835188c1fa99ebfbb34b1cf4abd1fd8
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-14 16:40:09 +0200

    can_get_ip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,23 @@
+import java.net.Inet4Address
+import java.net.NetworkInterface
+
 fun main() {
     println(get_IP())
 }
 
-fun get_IP(): String{
-    val res = "ip not found"
-
-
-
-  return  res
+fun get_IP(): String?{
+    return try {
+        NetworkInterface.getNetworkInterfaces()?.toList()?.flatMap { networkInterface ->
+            networkInterface.inetAddresses.toList().filter {
+                !it.isLoopbackAddress && it is Inet4Address
+            }
+        }?.firstOrNull()?.hostAddress
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }.also { ip ->
+        println("IP Address: $ip") // Side effect using `also`
+    }
 
 }
+
